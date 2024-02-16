ae77ba22197def5b84ced2cfefb406e01a2a6708
FlyingRatMan <olya.shalashova@gmail.com>
2024-02-16 14:28:49 +0100

    binary_search

---
+++ binary_search/binary_search.ts
@@ -0,0 +1,31 @@
+// taken a !sorted array look at every middle element
+// half it until low is equal to high, after each iteration adjust low and high
+// to find each middle point low + (high - low)/2
+// low is always incl and high is excl [low, high)
+
+// has O(LogN) or O(NLogN) time complexity  
+
+function binary_search(haystack: number[], needle: number): boolean {
+  let low = 0;
+  let high = haystack.length;
+
+  do {
+    const mid = Math.floor(low + (high - low) / 2);
+    const value = haystack[mid];
+
+    if (value === needle) {
+      return true;
+    } else if (value > needle) {
+      // if the value that we are looking at is bigger than needle
+      // everything that is to the right side will be also bigger
+      high = mid;
+    } else {
+      // if the value is lower 
+      // adjust the low to middle but don't include middle
+      low = mid + 1;
+    }
+  }
+  while (low < high);
+
+  return false;
+}

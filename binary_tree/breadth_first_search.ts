e5a43d0fe65cb57fe9b1a62977951336bf6c3f7f
FlyingRatMan <olya.shalashova@gmail.com>
2024-02-27 23:23:25 +0100

    binary_tree breadth first

---
+++ binary_tree/breadth_first_search.ts
@@ -0,0 +1,22 @@
+import type { BinaryNode } from "./BinaryNode.ts" 
+
+function breadth_first_search(head: BinaryNode<number>, needle: number): boolean {
+  const queue = [head];
+
+  while (queue.length) {
+    const curr = queue.shift() as BinaryNode<number>;
+
+    if (curr.value === needle) {
+      return true;
+    }
+
+    if (curr.left) {
+      queue.push(curr.left);
+    }
+    if (curr.right) {
+      queue.push(curr.right);
+    }
+  }
+
+  return false;
+}
